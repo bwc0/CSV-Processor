5f872c33680c9c4387ac64f6946eb06328336c4e
bwc0 <bryantcason32@gmail.com>
2018-09-30 18:59:55 -0400

    added writer configuration

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Simple Csv-Reader
+# Simple Csv-Processor
+
+## FlatFileWriter:
+
+Read multiple files from file location and write to one file
--- src/main/java/com/csvreader/config/BatchConfig.java
+++ src/main/java/com/csvreader/config/BatchConfig.java
@@ -9,9 +9,13 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.MultiResourceItemReader;
+import org.springframework.batch.item.file.MultiResourceItemWriter;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -28,6 +32,7 @@ public class BatchConfig {
 
     @Value("files/*.csv")
     private Resource[] inputResources;
+    private Resource outputResource = new FileSystemResource("output/outputData.csv");
 
     public BatchConfig(JobBuilderFactory jobs, StepBuilderFactory steps) {
         this.jobs = jobs;
@@ -48,6 +53,29 @@ public class BatchConfig {
                 .reader(multiResourceItemReader())
                 .writer(writer())
                 .build();
+    }
+
+    @Bean
+    public FlatFileItemWriter<Employee> writer() {
+        //Create write instance
+        FlatFileItemWriter<Employee> writer = new FlatFileItemWriter<>();
+        //Set output file location
+        writer.setResource(outputResource);
+        //All job repetitions should "append" to same output file
+        writer.setAppendAllowed(true);
+        //Name field values sequence based on object properties
+        writer.setLineAggregator(new DelimitedLineAggregator<Employee>() {
+            {
+                setDelimiter(",");
+                setFieldExtractor(new BeanWrapperFieldExtractor<Employee>() {
+                    {
+                        setNames(new String[] {"id", "firstName", "lastName"});
+                    }
+                });
+            }
+        });
+
+        return writer;
     }
 
     @Bean
@@ -84,10 +112,5 @@ public class BatchConfig {
             }
         });
         return reader;
-    }
-
-    @Bean
-    public ConsoleItemWriter<Employee> writer() {
-        return new ConsoleItemWriter<>();
     }
 }
